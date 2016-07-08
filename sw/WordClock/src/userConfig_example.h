1b6ff32c373167036d4f707b360615e89cf4aa50
Iain Chalmers <iain@chalmers.cc>
2016-07-08 11:54:07 +0100

    Adding an example userConfig.h, but making sure the actual file doesnt get added to git as it contains sensitive data e.g. wifi SSID and password

---
+++ sw/WordClock/src/userConfig_example.h
@@ -0,0 +1,9 @@
+// Change as appropriate for your setup, and save as "userConfig.h"
+#ifndef USERCONFIG_H
+#define USERCONFIG_H
+
+const char* ssid = "";
+const char* password = "";
+
+
+#endif  //USERCONFIG_H
