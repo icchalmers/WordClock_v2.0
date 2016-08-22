f653b3b9425b209f771aae448dd33200df5e2671
Iain Chalmers <iain@chalmers.cc>
2016-08-22 18:43:23 +0100

    Stle cleanup

--- sw/WordClock/src/main.cpp
+++ sw/WordClock/src/main.cpp
@@ -9,7 +9,9 @@
 *
 * LINKS :
 *     https://github.com/icchalmers/WordClock_v2.0
-*     http://fabacademy.org/archives/2015/eu/students/chalmers.iain/final-project-summary-wordclock20.html
+*
+*
+* http://fabacademy.org/archives/2015/eu/students/chalmers.iain/final-project-summary-wordclock20.html
 *
 * AUTHOR : Iain Chalmers
 *
@@ -30,43 +32,44 @@
 #include "./OTA.h"
 #include "./display.h"
 
-extern const char* ssid;
-extern const char* password;
-extern const char* MDNSName;
+extern const char *ssid;
+extern const char *password;
+extern const char *MDNSName;
 extern const uint16_t pixel_count;
 
 WS2812bDisplay display(pixel_count);
 
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    // Debug LED
-    DEBUG_LED_OFF
+  // Debug LED
+  DEBUG_LED_OFF
     pinMode(DEBUG_LED, OUTPUT);
 
-    display.init();
+  display.init();
 
-    Serial.println("Booting");
-    display.setStatus(DISPLAY_STATUS_INIT);
+  Serial.println("Booting");
+  display.setStatus(DISPLAY_STATUS_INIT);
 
-    // Start the wifi manager. If the clock can't connect to a wifi network it
-    // connected to previosuly, it puts up it's own AP called "WordClock". When
-    // the user connects to it, the captive portal takes them to a config page
-    // so that they can configure the wifi. The portal stays open for 10
-    // minutes.
-    WiFiManager wifiManager;
-    wifiManager.setTimeout(600);  // 600s = 10 minutes
-    if (!wifiManager.autoConnect("WordClock")) {
-      Serial.println("Failed to connect and timeout reached");
-      display.setStatus(DISPLAY_STATUS_WIFI_DISCONNECTED);
-    } else {
-      display.setStatus(DISPLAY_STATUS_OK);
-      Serial.println("Ready");
-    }
+  // Start the wifi manager. If the clock can't connect to a wifi network it
+  // connected to previosuly, it puts up it's own AP called "WordClock". When
+  // the user connects to it, the captive portal takes them to a config page
+  // so that they can configure the wifi. The portal stays open for 10
+  // minutes.
+  WiFiManager wifiManager;
+  wifiManager.setTimeout(600); // 600s = 10 minutes
 
-    ota_init(MDNSName);
+  if (!wifiManager.autoConnect("WordClock")) {
+    Serial.println("Failed to connect and timeout reached");
+    display.setStatus(DISPLAY_STATUS_WIFI_DISCONNECTED);
+  } else {
+    display.setStatus(DISPLAY_STATUS_OK);
+    Serial.println("Ready");
+  }
+
+  ota_init(MDNSName);
 }
 
 void loop() {
-    ArduinoOTA.handle();  // must be called frequently
+  ArduinoOTA.handle(); // must be called frequently
 }
